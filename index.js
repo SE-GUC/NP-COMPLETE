847d7473a9a5140f172165a4cb225b9d75210c07
Mohamed Hosam <medo.hosam1@yahoo.com>
2019-02-27 16:46:35 +0200

    read and delete admin

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ const app = express();
 app.use(express.json());
 
 const externalEntities = require('./routes/api/externalEntities');
+const admins = require('./routes/api/admins')
 
 
 
 app.get('/',(req,res) => {
     res.send(`<h1>WelcomeEE</h1>
-    <a href="/api/externalEntities">EE</a></br>`)   
+    <a href="/api/externalEntities">EE</a></br>
+    <a href="/api/admins">Admins</a>`)   
   })
   // Direct routes to appropriate files 
 app.use('/api/externalEntities',externalEntities);
+app.use('/api/admins' , admins)
+
 // Handling 404
 app.use((req, res) => {
   res.status(404).send({err: 'We can not find what you are looking for'});
