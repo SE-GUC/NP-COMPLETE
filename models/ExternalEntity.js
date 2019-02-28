23f4df63bb69fa76c7817e551a830efb7d340559
Mark-Micheal <36182888+Mark-Micheal@users.noreply.github.com>
2019-03-01 00:38:51 +0200

    Add files via upload

---
+++ models/ExternalEntity.js
@@ -0,0 +1,12 @@
+const uuidv4 = require('uuid/v4')
+
+class ExternalEntity {
+  constructor (fullName, email, phone) {
+    this.id = uuidv4()
+    this.fullName = fullName
+    this.email = email
+    this.phone = phone
+  }
+}
+
+module.exports = ExternalEntity
