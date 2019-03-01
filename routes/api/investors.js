7868995b22f486765b751177c6ceaf1fc6c068f2
M. Farid <mohamed.aiman@student.guc.edu.eg>
2019-03-01 14:30:56 +0200

    Followed newly added Conventions

--- routes/api/investors.js
+++ routes/api/investors.js
@@ -1,126 +1,130 @@
-//Dependancies
-const express = require('express');
-const Joi = require('joi');
-const uuid = require('uuid');
-const router = express.Router();
+// Dependancies
+const express = require('express')
+const Joi = require('joi')
+const uuidv4 = require('uuid/v4')
+const router = express.Router()
 
+// Investor model
+const Investor = require('../../models/Investor')
 
-//Investor model
-const Investor = require('../../models/Investor');
-
-
-// temporary data created as if it was pulled out of the database
+// Temporary data created as if it was pulled out of the database
 const investors = [
-	new Investor('Bill Gates', 30, 'microsoft@hotmail.com'),
-    new Investor('Alan Turing', 36, 'alan@yahoo.com'),
-    new Investor('Ahmed', 20, 'ahmed20@gmail.com'),
-    new Investor('Omar', 17, 'omar@outlook.com')
-];
+  new Investor('Bill Gates', 30, 'microsoft@hotmail.com'),
+  new Investor('Alan Turing', 36, 'alan@yahoo.com'),
+  new Investor('Ahmed', 20, 'ahmed20@gmail.com'),
+  new Investor('Omar', 17, 'omar@outlook.com')
+]
 
+// Default route ..returns all Investors in the array.
+router.get('/', (req, res) => res.json({ data: investors }))
 
-// Default route ..returns all investors in the array.
-router.get('/', (req, res) => res.json({data:investors}));
-
-//Return a specific investor with ID in URL. 
-router.get('/:id', (req,res)=> 
-{   
-    const investorId = req.params.id;
-    const returned = investors.find(investor => investor.id === investorId)
-    res.json(returned);
-})
-
-// Create a new investor
+// Create a new Investor
 router.post('/joi', (req, res) => {
-    const name = req.body.name;
-    const age = req.body.age;
-    const email = req.body.email;
+  const name = req.body.name
+  const age = req.body.age
+  const email = req.body.email
 
-    const schema = {
-     
-        name: Joi.string().min(4).required(),
-        age: Joi.number().required(),
-        email: Joi.string().required()
+  const schema = {
 
-    }
+    name: Joi.string().min(4).required(),
+    age: Joi.number().required(),
+    email: Joi.string().required()
 
-    const result = Joi.validate(req.body,schema);
+  }
 
-    if(result.error) return res.status(400).send({error: result.error.details[0].message});
+  const result = Joi.validate(req.body, schema)
 
+  if (result.error) return res.status(400).send({ error: result.error.details[0].message })
 
-	const newInvestor = {
-        name,
-        age,
-        email,
-        id: uuid.v4(),
-    };
+  const newInvestor = {
+    name,
+    age,
+    email,
+    id: uuidv4()
+  }
 
-    investors.push(newInvestor);
-	return res.json({ data: newInvestor});
-});
+  investors.push(newInvestor)
+  return res.json({ data: newInvestor })
+})
 
-//Update an existing Investor with joi validation
+// Reads a specific Investor given id in URL.
+router.get('/:id', (req, res) => {
+  const investorId = req.params.id
+  const investor = investors.find(investor => investor.id === investorId)
+  if (investor) {
+    res.json({ data: investor })
+  } else {
+    res.status(400).json({ status: 'error',
+      message: 'Investor not found',
+      data: investors })
+  }
+})
+
+// Update an existing Investor with joi validation
 router.put('/:id', (req, res) => {
+  const id = req.params.id
+  const investorToBeUpdated = investors.find(Investor => Investor.id === id)
+  const newName = req.body.name
+  const newAge = req.body.age
+  const newEmail = req.body.email
 
-    const id = req.params.id;
-    const investorToBeUpdated = investors.find(Investor => Investor.id === id);
-    const newName = req.body.name;
-    const newAge = req.body.age;
-    const newEmail = req.body.email;
-
-    if(newName)
-    {
-        const nameSchema = 
+  if (newName) {
+    const nameSchema =
         {
-            name: Joi.string().min(4).required(),
+          name: Joi.string().min(4).required()
         }
 
-        const resultName = Joi.validate(req.body,nameSchema);
-        if(resultName.error) return res.status(400).send({error: resultName.error.details[0].message});
-       
-        investorToBeUpdated.name = newName;
-    }
+    const resultName = Joi.validate(req.body, nameSchema)
+    if (resultName.error) return res.status(400).send({ error: resultName.error.details[0].message })
 
-    if(newAge)
-    {
-        const ageSchema = 
+    investorToBeUpdated.name = newName
+  }
+
+  if (newAge) {
+    const ageSchema =
         {
-            age: Joi.number().required(),
+          age: Joi.number().required()
         }
 
-        const resultAge = Joi.validate(req.body,ageSchema);
-        if(resultAge.error) return res.status(400).send({error: resultAge.error.details[0].message});
-        
-        investorToBeUpdated.age = newAge;
+    const resultAge = Joi.validate(req.body, ageSchema)
+    if (resultAge.error) return res.status(400).send({ error: resultAge.error.details[0].message })
 
-    }
+    investorToBeUpdated.age = newAge
+  }
 
-    if(newEmail)
-    {
-        const emailSchema = 
+  if (newEmail) {
+    const emailSchema =
         {
-            email: Joi.string().required()
+          email: Joi.string().required()
         }
 
-        const resultEmail = Joi.validate(req.body,emailSchema);
-        if(resultEmail.error) return res.status(400).send({error: resultEmail.error.details[0].message});
-        
-        investorToBeUpdated.email = newEmail;
-    }
+    const resultEmail = Joi.validate(req.body, emailSchema)
+    if (resultEmail.error) return res.status(400).send({ error: resultEmail.error.details[0].message })
 
-    res.send(investors);
+    investorToBeUpdated.email = newEmail
+  }
 
+  res.send(investors)
 })
 
-//Delete a specific investor with ID in URL 
-router.delete('/:id', (req,res)=> 
-{   
-    const investorId = req.params.id;
-    const index = investors.findIndex(investor => investor.id === investorId);
-    investors.splice(index,1)
-    res.send(investors);
+// Delete a specific Investor given ID in URL
+router.delete('/:id', (req, res) => {
+  const investorId = req.params.id
+  const investor = investors.find(investor => investor.id === investorId)
+  if (investor) {
+    const index = investors.indexOf(investor)
+    investors.splice(index, 1)
+    res.json({ data: investors })
+  } else {
+    res.status(400).json({ status: 'error',
+      message: 'Investor not found',
+      data: investors })
+  }
 })
 
-module.exports = router;
+//! Error route needed
+router.use((req, res) => {
+  res.status(404).send({ err: 'We can not find what you are looking for' })
+})
 
-
+module.exports = router
