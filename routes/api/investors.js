a6f63aba8fb470bcde590137ec62235730133489
MohamedAyman98 <mohamedayman98@github.com>
2019-03-01 16:56:50 +0200

    Applied conventions

--- routes/api/investors.js
+++ routes/api/investors.js
@@ -9,42 +9,41 @@ const Investor = require('../../models/Investor')
 
 // Temporary data created as if it was pulled out of the database
 const investors = [
-  new Investor('Bill Gates', 30, 'microsoft@hotmail.com'),
-  new Investor('Alan Turing', 36, 'alan@yahoo.com'),
-  new Investor('Ahmed', 20, 'ahmed20@gmail.com'),
-  new Investor('Omar', 17, 'omar@outlook.com')
+  new Investor('Mohamed Ayman','1998-10-16', 20,'mohamedAyman@gmail.com'),
+  new Investor('Mohamed Farid','1998-12-18', 20,'mohamedFarid@hotmail.com'),
+  new Investor('Bill Marks','1990-05-21', 28,'billMarks@outlook.com')
 ]
 
 // Default route ..returns all Investors in the array.
 router.get('/', (req, res) => res.json({ data: investors }))
 
 // Create a new Investor
-router.post('/joi', (req, res) => {
-  const name = req.body.name
-  const age = req.body.age
-  const email = req.body.email
+router.post('/', (req, res) => {
+  const data = req.body
+  const schema = Joi.object().keys({
+   
+    fullName: Joi.string().min(4).required(),
+    birthDate: Joi.date().required().iso(),
+    email: Joi.string().email().required()
+    
+  })
 
-  const schema = {
-
-    name: Joi.string().min(4).required(),
-    age: Joi.number().required(),
-    email: Joi.string().required()
-
-  }
-
-  const result = Joi.validate(req.body, schema)
-
-  if (result.error) return res.status(400).send({ error: result.error.details[0].message })
-
-  const newInvestor = {
-    name,
-    age,
-    email,
-    id: uuidv4()
-  }
-
-  investors.push(newInvestor)
-  return res.json({ data: newInvestor })
+  Joi.validate(data, schema, (error, value) => {
+    if (error) {
+      return res.status(400).json({
+        status: 'error',
+        message: error.details[0].message,
+        data: data
+      })
+    }
+   const newInvestor = new Investor(value.fullName,value.birthDate,value.email)
+   investors.push(newInvestor)
+   return res.json({
+    status: 'success',
+    message: `New Investor created with id ${newInvestor.id}`,
+    data: newInvestor
+  })
+})
 })
 
 // Reads a specific Investor given id in URL.
@@ -62,50 +61,47 @@ router.get('/:id', (req, res) => {
 
 // Update an existing Investor with joi validation
 router.put('/:id', (req, res) => {
+  const data = req.body
+  if(Object.keys(data).length === 0)
+    return res.status(400).json({status : 'error', message: 'No data to update'})
+  
+  const schema = Joi.object().keys({
+   
+    fullName: Joi.string().min(4),
+    birthDate: Joi.date().iso(),
+    email: Joi.string().email() 
+  })
+
+  Joi.validate(data, schema, (error, value) => {
+    if (error)
+      return res.status(400).json({
+        status: 'error',
+        message: error.details[0].message,
+        data: data
+      })
+    
+
   const id = req.params.id
   const investorToBeUpdated = investors.find(Investor => Investor.id === id)
-  const newName = req.body.name
-  const newAge = req.body.age
-  const newEmail = req.body.email
 
-  if (newName) {
-    const nameSchema =
-        {
-          name: Joi.string().min(4).required()
-        }
+  if(!investorToBeUpdated)
+    res.status(400).json({status: 'error', message: 'No investor was found with this ID'})
 
-    const resultName = Joi.validate(req.body, nameSchema)
-    if (resultName.error) return res.status(400).send({ error: resultName.error.details[0].message })
+    Object.keys(value).forEach(key => {
+      if (value[key]) {
+        investorToBeUpdated[key] = value[key]
+      }
+    })
 
-    investorToBeUpdated.name = newName
-  }
-
-  if (newAge) {
-    const ageSchema =
-        {
-          age: Joi.number().required()
-        }
-
-    const resultAge = Joi.validate(req.body, ageSchema)
-    if (resultAge.error) return res.status(400).send({ error: resultAge.error.details[0].message })
-
-    investorToBeUpdated.age = newAge
-  }
-
-  if (newEmail) {
-    const emailSchema =
-        {
-          email: Joi.string().required()
-        }
-
-    const resultEmail = Joi.validate(req.body, emailSchema)
-    if (resultEmail.error) return res.status(400).send({ error: resultEmail.error.details[0].message })
-
-    investorToBeUpdated.email = newEmail
-  }
-
-  res.send(investors)
+     return res.json({
+      status: 'success',
+      message: `Updated investor with id ${id}`,
+      data: investorToBeUpdated
+    })
+  })
 })
+
+  
 
 // Delete a specific Investor given ID in URL
 router.delete('/:id', (req, res) => {
