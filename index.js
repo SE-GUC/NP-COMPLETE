64507f62b22b837f5f8415f6a97f76451441753a
Lujine Elfeky <lujineelfeky@gmail.com>
2019-03-01 20:25:15 +0200

    Modified index.js to match with the current in dev

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const express = require('express')
-const app = express()
-
-app.use(express.json())
-
+// const externalEntities = require('./routes/api/externalEntities')
+// const admins = require('./routes/api/admins')
+// const investors = require('./routes/api/investors')
+// const lawyers = require('./routes/api/lawyers')
+// const reviewers = require('./routes/api/reviewers')
 const companies = require('./routes/api/companies')
 
+const app = express()
+app.use(express.json())
+
 app.get('/', (req, res) => {
-  res.send(`<h1>WelcomeEE</h1>`)
+  res.send(`<h1>WelcomeEE</h1>
+    // <a href="/api/externalEntities">EE</a></br>
+    // <a href="/api/admins">Admins</a>
+    // <a href="/api/lawyers">lawyers</a></br>,
+    // <a href="/api/investors">Investors</a></br>
+    // <a href="/api/reviewers">Reviewers</a></br>
+    <a href="/api/companies">Companies</a></br>`)
 })
 
 // Direct routes to appropriate files
+// app.use('/api/externalEntities', externalEntities)
+// app.use('/api/admins', admins)
+// app.use('/api/investors', investors)
+// app.use('/api/reviewers', reviewers)
+// app.use('/api/lawyers', lawyers)
 app.use('/api/companies', companies)
 
 // Handling 404
@@ -17,5 +32,5 @@ app.use((req, res) => {
   res.status(404).send({ err: 'We can not find what you are looking for' })
 })
 
-const port = 8000
-app.listen(port, () => { console.log(`Server is running on port ${port}`) })
+const port = process.env.port | 8000
+app.listen(port, () => { console.log(`Server is up and running on port ${port}`) })
