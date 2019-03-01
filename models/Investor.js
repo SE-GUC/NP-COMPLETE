4c79620ed5b28ad7505742101ea52d900b8eb0cf
M. Farid <mohamed.aiman@student.guc.edu.eg>
2019-03-01 17:09:44 +0200

    Style change and unused imports
    Removed uuid/v4 and applied style

--- models/Investor.js
+++ models/Investor.js
@@ -1,15 +1,13 @@
-const uuidv4 = require('uuid/v4');
+const uuidv4 = require('uuid/v4')
 
 class Investor {
-    constructor(fullName, birthDate, age, email) {
-        this.fullName = fullName;
-        this.birthDate = birthDate;
-        this.age = this.age = (new Date()).getFullYear() - (new Date(birthDate)).getFullYear();
-        this.email = email;
-        this.id = uuidv4();
-        
-    };
+  constructor (fullName, birthDate, age, email) {
+    this.fullName = fullName
+    this.birthDate = birthDate
+    this.age = this.age = (new Date()).getFullYear() - (new Date(birthDate)).getFullYear()
+    this.email = email
+    this.id = uuidv4()
+  };
 }
 
 module.exports = Investor
-
--- routes/api/investors.js
+++ routes/api/investors.js
@@ -1,7 +1,6 @@
 // Dependancies
 const express = require('express')
 const Joi = require('joi')
-const uuidv4 = require('uuid/v4')
 const router = express.Router()
 
 // Investor model
@@ -9,9 +8,9 @@ const Investor = require('../../models/Investor')
 
 // Temporary data created as if it was pulled out of the database
 const investors = [
-  new Investor('Mohamed Ayman','1998-10-16', 20,'mohamedAyman@gmail.com'),
-  new Investor('Mohamed Farid','1998-12-18', 20,'mohamedFarid@hotmail.com'),
-  new Investor('Bill Marks','1990-05-21', 28,'billMarks@outlook.com')
+  new Investor('Mohamed Ayman', '1998-10-16', 20, 'mohamedAyman@gmail.com'),
+  new Investor('Mohamed Farid', '1998-12-18', 20, 'mohamedFarid@hotmail.com'),
+  new Investor('Bill Marks', '1990-05-21', 28, 'billMarks@outlook.com')
 ]
 
 // Default route ..returns all Investors in the array.
@@ -21,11 +20,11 @@ router.get('/', (req, res) => res.json({ data: investors }))
 router.post('/', (req, res) => {
   const data = req.body
   const schema = Joi.object().keys({
-   
+
     fullName: Joi.string().min(4).required(),
     birthDate: Joi.date().required().iso(),
     email: Joi.string().email().required()
-    
+
   })
 
   Joi.validate(data, schema, (error, value) => {
@@ -36,14 +35,14 @@ router.post('/', (req, res) => {
         data: data
       })
     }
-   const newInvestor = new Investor(value.fullName,value.birthDate,value.email)
-   investors.push(newInvestor)
-   return res.json({
-    status: 'success',
-    message: `New Investor created with id ${newInvestor.id}`,
-    data: newInvestor
+    const newInvestor = new Investor(value.fullName, value.birthDate, value.email)
+    investors.push(newInvestor)
+    return res.json({
+      status: 'success',
+      message: `New Investor created with id ${newInvestor.id}`,
+      data: newInvestor
+    })
   })
-})
 })
 
 // Reads a specific Investor given id in URL.
@@ -62,30 +61,28 @@ router.get('/:id', (req, res) => {
 // Update an existing Investor with joi validation
 router.put('/:id', (req, res) => {
   const data = req.body
-  if(Object.keys(data).length === 0)
-    return res.status(400).json({status : 'error', message: 'No data to update'})
-  
+  if (Object.keys(data).length === 0) { return res.status(400).json({ status: 'error', message: 'No data to update' }) }
+
   const schema = Joi.object().keys({
-   
+
     fullName: Joi.string().min(4),
     birthDate: Joi.date().iso(),
-    email: Joi.string().email() 
+    email: Joi.string().email()
   })
 
   Joi.validate(data, schema, (error, value) => {
-    if (error)
+    if (error) {
       return res.status(400).json({
         status: 'error',
         message: error.details[0].message,
         data: data
       })
-    
+    }
 
-  const id = req.params.id
-  const investorToBeUpdated = investors.find(Investor => Investor.id === id)
+    const id = req.params.id
+    const investorToBeUpdated = investors.find(Investor => Investor.id === id)
 
-  if(!investorToBeUpdated)
-    res.status(400).json({status: 'error', message: 'No investor was found with this ID'})
+    if (!investorToBeUpdated) { res.status(400).json({ status: 'error', message: 'No investor was found with this ID' }) }
 
     Object.keys(value).forEach(key => {
       if (value[key]) {
@@ -93,15 +90,13 @@ router.put('/:id', (req, res) => {
       }
     })
 
-     return res.json({
+    return res.json({
       status: 'success',
       message: `Updated investor with id ${id}`,
       data: investorToBeUpdated
     })
   })
 })
-
-  
 
 // Delete a specific Investor given ID in URL
 router.delete('/:id', (req, res) => {
