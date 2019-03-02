613ab7c812c8cab394be7785fecd79577b9aa2a3
Mohamed Hosam <medo.hosam1@yahoo.com>
2019-03-02 15:10:16 +0200

    changed birthDate to birthdate

--- models/Admin.js
+++ models/Admin.js
@@ -2,12 +2,12 @@
 const uuidv4 = require('uuid/v4')
 
 class Admin {
-  constructor (fullName, email, birthDate, startDate, workingHours, salary) {
+  constructor (fullName, email, birthdate, startDate, workingHours, salary) {
     this.id = uuidv4()
     this.fullName = fullName
     this.email = email
-    this.birthDate = birthDate
-    this.age = (new Date()).getFullYear() - (new Date(birthDate)).getFullYear()
+    this.birthdate = birthdate
+    this.age = (new Date()).getFullYear() - (new Date(birthdate)).getFullYear()
     this.startDate = startDate
     this.workingHours = workingHours
     this.salary = salary
