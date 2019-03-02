404731e5243aa16a1d06acc81f4cd579420fbccd
Adeeel98 <mohamed.naguibahmed@student.guc.edu.eg>
2019-03-02 15:21:01 +0200

    change the position of if of investor

--- models/Investor.js
+++ models/Investor.js
@@ -2,11 +2,11 @@ const uuidv4 = require('uuid/v4')
 
 class Investor {
   constructor (fullName, birthdate, email) {
+    this.id = uuidv4()
     this.fullName = fullName
     this.birthdate = birthdate
     this.email = email
-    this.id = uuidv4()
-  };
+  }
 }
 
 module.exports = Investor
