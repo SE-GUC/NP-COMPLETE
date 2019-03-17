9c0750d7d2663df56108b26554956ffa213507a8
Mark-Micheal <markmichealhp@outlook.com>
2019-03-17 22:36:21 +0200

    added .json response to delete

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -21,13 +21,6 @@ router.get('/:id', async (req, res) => {
   } else {
     res.json({ data: task })
   }
-  /* if (task) {
-    res.json({ data: task })
-  } else {
-    res.status(400).json({ status: 'error',
-      message: 'Task not found',
-      data: tasks })
-  } */
 })
 
 // create a task
@@ -41,37 +34,7 @@ router.post('/', async (req, res) => {
     console.log(error)
   }
 })
-/* router.post('/', (req, res) => {
-  const data = req.body
-  const schema = Joi.object().keys({
-    handler: Joi.string().required(),
-    creationDate: Joi.date().iso().required(),
-    deadline: Joi.date().required().iso()
-  })
 
-  Joi.validate(data, schema, (err, value) => {
-    if (err) {
-      return res.status(400).json({
-        status: 'Error',
-        message: err.details[0].message,
-        data: data
-      })
-    }
-
-    const newTask = new Task(
-      value.handler,
-      value.creationDate,
-      value.deadline
-    )success',
-      message
-    tasks.push(newTask)
-    return res.json({
-      status: ': `New Task created with id ${newTask.id}`,
-      data: newTask
-    })
-  })
-})
-*/
 // update a task
 router.put('/:id', async (req, res) => {
   try {
@@ -84,69 +47,16 @@ router.put('/:id', async (req, res) => {
     console.log(error)
   }
 })
-/* router.put('/:id', (req, res) => {
-  const data = req.body
-  if (Object.keys(data).length === 0) {
-    return res.status(400).json({
-      status: 'Error',
-      message: 'No data to update'
-    })
-  }
-
-  const schema = Joi.object().keys({
-    handler: Joi.string(),
-    creationDate: Joi.date().iso(),
-    deadline: Joi.date().iso()
-  })
-
-  Joi.validate(data, schema, (err, value) => {
-    if (err) {
-      return res.status(400).json({
-        status: 'Error',
-        message: err.details[0].message,
-        data: data
-      })
-    }
-
-    const id = req.params.id
-    const taskToUpdate = tasks.find(task => task.id === id)
-
-    if (!taskToUpdate) {
-      return res.status(400).json({
-        status: 'Error',
-        message: 'Task not found',
-        availableTasks: tasks
-      })
-    }
-
-    Object.keys(value).forEach(key => {
-      if (value[key]) {
-        taskToUpdate[key] = value[key]
-      }
-    })
-
-    return res.json({
-      status: 'Success',
-      message: `Updated task with id ${id}`,
-      data: taskToUpdate
-    })
-  })
-})
-*/
 
 // Delete a Task
 router.delete('/:id', async (req, res) => {
   try {
     const taskId = req.params.id
     await Task.findByIdAndRemove({ _id: taskId })
+    res.json({ msg: 'Task deleted successfully' })
   } catch (error) {
     console.log(error)
   }
-  /* const taskId = req.params.id
-  const task = tasks.find(tasks => tasks.id === taskId)
-  const index = tasks.indexOf(task)
-  tasks.splice(index, 1)
-  res.send(tasks) */
 })
 
 module.exports = router
