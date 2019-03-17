3bcc139cc58b9fe9f1374c260294712b7e77057e
Mark-Micheal <markmichealhp@outlook.com>
2019-03-17 20:36:16 +0200

    updated tasks.js to work with mongoDB

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -1,34 +1,47 @@
 const express = require('express')
-const Joi = require('joi')
 const router = express.Router()
+const mongoose = require('mongoose')
 
 // Task model
 const Task = require('../../models/Task')
-
-// temporary data (to act as a mock database)
-const tasks = [
-  new Task('Mohamed Hosam', '1998-01-22', '2019-01-01'),
-  new Task('Yasser', '1998-06-05', '2018-05-03')
-]
+const validator = require('../../validations/taskValidations')
 
 // Read all Tasks
-router.get('/', (req, res) => res.json({ data: tasks }))
+router.get('/', async (req, res) => {
+  const tasks = await Task.find()
+  res.json({ data: tasks })
+})
 
 // Read specific task
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   const taskId = req.params.id
-  const task = tasks.find(tasks => tasks.id === taskId)
-  if (task) {
+  const task = await Task.findOne({ taskId })
+  if (!task) {
+    return res.status(404).send({ error: 'Book does not exist' })
+  } else {
+    res.json({ data: task })
+  }
+  /* if (task) {
     res.json({ data: task })
   } else {
     res.status(400).json({ status: 'error',
       message: 'Task not found',
       data: tasks })
-  }
+  } */
 })
 
 // create a task
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const isValidated = validator.createValidation(req.body)
+    if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
+    const newTask = await Task.create(req.body)
+    res.json({ msg: 'Task was created successfully', data: newTask })
+  } catch (error) {
+    console.log(error)
+  }
+})
+/* router.post('/', (req, res) => {
   const data = req.body
   const schema = Joi.object().keys({
     handler: Joi.string().required(),
@@ -49,18 +62,29 @@ router.post('/', (req, res) => {
       value.handler,
       value.creationDate,
       value.deadline
-    )
+    )success',
+      message
     tasks.push(newTask)
     return res.json({
-      status: 'success',
-      message: `New Task created with id ${newTask.id}`,
+      status: ': `New Task created with id ${newTask.id}`,
       data: newTask
     })
   })
 })
-
+*/
 // update a task
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  try {
+    const taskId = req.params.id
+    const task = await Task.findOne({ taskId })
+    if (!task) return res.status(404).send({ error: 'Task does not exist' })
+    const updatedTask = await Task.updateOne(req.body)
+    res.json({ msg: 'Task updated successfully' })
+  } catch (error) {
+    console.log(error)
+  }
+})
+/* router.put('/:id', (req, res) => {
   const data = req.body
   if (Object.keys(req.body).length === 0) {
     return res.status(400).json({
@@ -107,14 +131,21 @@ router.put('/:id', (req, res) => {
     })
   })
 })
+*/
 
 // Delete a Task
-router.delete('/:id', (req, res) => {
-  const taskId = req.params.id
+router.delete('/:id', async (req, res) => {
+  try {
+    const taskId = req.params.id
+    const deletedTask = await Task.findByIdAndRemove(taskId)
+  } catch (error) {
+    console.log(error)
+  }
+  /* const taskId = req.params.id
   const task = tasks.find(tasks => tasks.id === taskId)
   const index = tasks.indexOf(task)
   tasks.splice(index, 1)
-  res.send(tasks)
+  res.send(tasks) */
 })
 
 module.exports = router
