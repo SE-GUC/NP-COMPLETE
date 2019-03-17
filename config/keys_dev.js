895a269e836ba7779f755949bb0bab7bf997b6e2
Mark-Micheal <markmichealhp@outlook.com>
2019-03-17 21:05:52 +0200

    install mongoose and updating keys

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://<username>:<password>@c<host>/<dbname>'
+  mongoURI: 'mongodb+srv://Mark_Micheal:<MarkMicheal99>@sumerge-1uwi5.mongodb.net/test?retryWrites=true'
 }
