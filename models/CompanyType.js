b1e3cb864d97cbc84570f830ba12e4ed92635bd3
Lujine Elfeky <lujineelfeky@gmail.com>
2019-03-20 21:21:22 +0200

    Added Company Type model

---
+++ models/CompanyType.js
@@ -0,0 +1,19 @@
+// The Company Type model
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const companyTypeSchema = new Schema({
+  companyType: {
+    type: String,
+    required: true
+  },
+  fields: {
+    type: [String],
+    required: true
+  },
+  types: {
+    type: [String],
+    required: true
+  }
+})
+module.exports = mongoose.model('companyType', companyTypeSchema)
