5c1908611749974b9b262f632f36dd3aa14d186e
Lujine Elfeky <lujineelfeky@gmail.com>
2019-03-21 13:09:52 +0200

    Added validations field to company type schema

--- models/CompanyType.js
+++ models/CompanyType.js
@@ -14,6 +14,10 @@ const companyTypeSchema = new Schema({
   types: {
     type: [String],
     required: true
+  },
+  validations: {
+    type: [String],
+    required: true
   }
 })
 module.exports = mongoose.model('companyType', companyTypeSchema)
