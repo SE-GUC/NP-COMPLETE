90b0ba4d911328fd8a25e9992042587fe12662aa
Adeeel98 <mohamed.naguibahmed@student.guc.edu.eg>
2019-03-22 22:25:41 +0200

    update the investors
    Adding user stories to the investors :
    1-filling the form
    2-get all the companies

--- routes/api/investors.js
+++ routes/api/investors.js
@@ -6,6 +6,8 @@ const router = express.Router()
 const Investor = require('../../models/Investor')
 const Company = require('../../models/Company')
 const validator = require('../../validations/investorValidations')
+const companyType = require('../../models/CompanyType')
+const companyValidator = require('../../validations/companyValidations')
 
 // Read all Investors (Default route)
 router.get('/', async (req, res) => {
@@ -164,5 +166,60 @@ router.get('/trackApplication/:id', async (req, res) => {
       console.error(err)
     })
 })
+// As an investor I should be able to view a list of my current or pending companies, so that I can access their details.
+router.get('/getCompanies/:id', async (req, res) => {
+  try {
+    const investorId = req.params.id
+    const query = { 'investorId': investorId }
+    const companies = await Company.find(query)
+    return res.json({
+      status: 'Success',
+      message: `companies`,
+      data: companies
+    })
+  } catch (error) {
+    console.log('error')
+  }
+})
+// As an investor I should be able to fill an application form, so that I can establish a company.
+router.post('/fillForm/:id', async (req, res) => {
+  try {
+    const isValidated = companyValidator.createValidation(req.body)
+    if (isValidated.error) {
+      return res.status(400).json({
+        status: 'Error',
+        message: isValidated.error.details[0].message
+      })
+    }
+    const type = req.body.type
+    const query = { 'companyType': type }
+    const companyTypeTemp = await companyType.find(query)
+    const fieldsTemp = companyTypeTemp[0].fields
+    const dataTypesArray = companyTypeTemp[0].types
+    const data = req.body.form.data
+    for (let i = 0; i < dataTypesArray.length; i++) {
+      const dataType = typeof (data[i])
+      if (!(dataType === dataTypesArray[i])) {
+        if (!(dataTypesArray[i] === 'date' && isValidDate(data[i]))) {
+          return res.status(400).json({
+            status: 'Error',
+            message: 'wrong data type: ' + fieldsTemp[i] + ' should be ' + dataTypesArray[i]
+          })
+        }
+      }
+    }
+    const newCompany = await Company.create(req.body)
+    return res.json({
+      status: 'You applied for establishing a new copmany',
+      data: newCompany
+    })
+  } catch (error) {
+    console.log('error')
+  }
+})
+const isValidDate = stringDate => {
+  const date = new Date(stringDate)
+  return date && Object.prototype.toString.call(date) === '[object Date]' && !isNaN(date)
+}
 
 module.exports = router
