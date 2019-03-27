c7e9263484e085e1b594ba6b12838b8d79a13db4
Mohamed Hosam <medo.hosam1@yahoo.com>
2019-03-27 22:12:00 +0200

    added tests for default

--- tests/task.test.js
+++ tests/task.test.js
@@ -1,5 +1,32 @@
 const task = require('./task')
 
+test('Read-multiple-Tasks exists', async () => {
+  expect.assertions(1)
+  return expect(typeof (task.default)).toBe('function')
+})
+
+test('Read multiple Tasks by id', async () => {
+  const data = {
+    department: 'Lawyer',
+    creationDate: '1837-02-15',
+    deadline: '1998-02-15'
+  }
+  const data1 = {
+    department: 'Lawyer',
+    creationDate: '1837-02-15',
+    deadline: '1998-02-15'
+  }
+  const created = await task.createTask(data)
+  const created1 = await task.createTask(data1)
+  const createdData = created.data.data
+  const createdData1 = created1.data.data
+  const read = await task.default()
+  const readData = read.data.data[0]
+  const readData1 = read.data.data[1]
+  expect.assertions(1)
+  return (expect(readData).toEqual(createdData) && expect(readData1).toEqual(createdData1))
+})
+
 test('Read-a-Task exists', async () => {
   expect.assertions(1)
   return expect(typeof (task.readTask)).toBe('function')
