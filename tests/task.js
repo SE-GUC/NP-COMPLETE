38071ed69ac30c3b95dd411bd53c211f765ee46c
Shiko <ahmedelsherif98j@gmail.com>
2019-03-29 05:20:01 +0200

    Update task.js

--- tests/task.js
+++ tests/task.js
@@ -1,5 +1,8 @@
 const axios = require('axios')
 const task = {
+    createTask: async (data) => {
+        return axios.post(`http://localhost:8000/api/tasks/`, data)
+      },
     viewDepartmentTask: async (data) => {
         return axios.put(`http://localhost:8000/api/tasks/viewDepartmentTask/`,data)
       }
