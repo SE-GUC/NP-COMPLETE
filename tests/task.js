0752bd9909c20020b13898d8d7c0a61c50f7cab1
MohamedTarawa <mohamed.tarawa@student.guc.edu.eg>
2019-03-30 18:05:34 +0200

    Story 6.01 JEST Tests ( needs reviewing )
    InternalUser: Reviewer

--- tests/task.js
+++ tests/task.js
@@ -1,7 +1,10 @@
 const axios = require('axios')
 const task = {
-    viewDepartmentTask: async (data) => {
-        return axios.put(`http://localhost:8000/api/tasks/viewDepartmentTask/`,data)
-      }
+  viewDepartmentTask: async (data) => {
+    return axios.put(`http://localhost:8000/api/tasks/viewDepartmentTask/`,data)
+  },
+  createTask: async (data) => {
+    return axios.post(`http://localhost:8000/api/tasks/`, data)
+  }
 }
-module.exports = task 
+module.exports = task
