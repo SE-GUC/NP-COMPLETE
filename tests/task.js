c4e6f2c9ba08bad6a5b282a62292e01a7f52d989
MohamedTarawa <mohamed.tarawa@student.guc.edu.eg>
2019-03-30 17:53:50 +0200

    Story 6.01 JEST Tests ( needs reviewing )

--- tests/task.js
+++ tests/task.js
@@ -2,6 +2,9 @@ const axios = require('axios')
 const task = {
   viewDepartmentTask: async (data) => {
     return axios.put(`http://localhost:8000/api/tasks/viewDepartmentTask/`, data)
+  },
+  createTask: async (data) => {
+    return axios.post(`http://localhost:8000/api/tasks/`, data)
   }
 }
 module.exports = task
