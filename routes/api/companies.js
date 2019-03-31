21f649c9e5e72d4299f0c72b8793de6a7549d260
Shiko <ahmedelsherif98j@gmail.com>
2019-03-31 02:22:45 +0200

    Update companies.js

--- routes/api/companies.js
+++ routes/api/companies.js
@@ -97,6 +97,7 @@ router.delete('/:id', async (req, res) => {
     const id = req.params.id
     const companyToBeDeleted = await Company.findByIdAndRemove(id)
     if (!companyToBeDeleted) {
+      const comppp = await Company.find()
       return res.status(400).json({
         status: 'Error',
         message: 'could not find Company you are looking for'
