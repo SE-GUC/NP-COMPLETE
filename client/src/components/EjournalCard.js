a4f2bb85070be612b2866b41b084e24c4077ed58
Adeeel98 <mohamed.naguibahmed@student.guc.edu.eg>
2019-04-05 05:26:00 +0200

    Show Ejornals

---
+++ client/src/components/EjournalCard.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import Card from 'react-bootstrap/Card'
+import CardDeck from 'react-bootstrap/CardDeck'
+export class EjournalCard extends Component {
+  render () {
+    return (
+      <div>
+        <CardDeck>
+          <Card>
+            <Card.Body>
+              <Card.Title>{this.props.data.name}</Card.Title>
+              {
+                Object.keys(this.props.data).splice(1, Object.keys.length + 1).map(item =>
+                  <Card.Text>
+                    {this.props.data[item]}
+                  </Card.Text>
+                )}
+            </Card.Body>
+          </Card>
+        </CardDeck>
+      </div>
+    )
+  }
+}
+
+export default EjournalCard
---
+++ client/src/components/Ejournals.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+// import Button from 'react-bootstrap/Button'
+// import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
+// import CardColumns from 'react-bootstrap/CardColumns'
+import EjournalCard from './EjournalCard'
+import axios from 'axios'
+
+export class Ejournals extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      ejornals: [],
+      ID: ''
+    }
+  }
+  componentDidMount () {
+    axios
+      .get('http://localhost:8000/api/users/showEstablishedCompanies')
+      .then(res => this.setState({ ejornals: res.data.data, ID: res.data._id }))
+      .catch(err => {
+        return this.setState({ error: err })
+      })
+  }
+
+  render () {
+    return (
+      this.state.ejornals.map(ejornal => {
+        return <EjournalCard data={ejornal} />
+      })
+    )
+  }
+}
+
+export default Ejournals
