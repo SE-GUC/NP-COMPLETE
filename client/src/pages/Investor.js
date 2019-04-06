1128f45f773cd66c034e1d7bcae36a081d8247f9
Shiko <ahmedelsherif98j@gmail.com>
2019-04-06 22:23:26 +0200

    Create Investor.js

---
+++ client/src/pages/Investor.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import Axios from 'axios'
+import DeleteAccounts from '../components/DeleteAccounts'
+import PropTypes from 'prop-types'
+
+
+export class Investor extends Component {
+    _isMounted = false
+    constructor(props) {
+        super(props)
+        this.state={
+          users:[]
+        }
+      }
+      deleteMe =id =>{
+            Axios
+            .delete(`https://shrouded-basin-67688.herokuapp.com/api/investors/${id}`)
+            .then(res =>{
+             this.setState({users:res.data.remainingInvestors})}) 
+            .catch(err => this.setState({error:true}))
+      }
+
+    componentDidMount() {
+        this._isMounted = true
+        Axios
+        .get('https://shrouded-basin-67688.herokuapp.com/api/investors')
+        .then(res => this.setState({ users: res.data.data }))
+        .catch(err => this.setState({ error: true }))
+    }
+
+    componentWillUnmount() {
+        this._isMounted = false
+      }
+  render() {
+    return this.state.error? <h1>process could not be completed</h1>:this.state.loading?
+    <h1>loading please be patient</h1>
+    :
+    ( <div className='Investor'>
+        <DeleteAccounts users = {this.state.users} deleteMe = {this.deleteMe} />
+      </div>
+    )
+  }
+}
+
+Investor.propTypes = {
+users: PropTypes.array
+}
+
+export default Investor
