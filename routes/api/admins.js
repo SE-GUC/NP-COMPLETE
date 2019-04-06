f819cf584d38a78bf25e4726602bdeef266bf035
mostafa-elhaiany <mostafa_elhaiany@outlook.com>
2019-04-06 21:27:44 +0200

    added routing to LogIn page

--- routes/api/admins.js
+++ routes/api/admins.js
@@ -48,41 +48,6 @@ router.get('/showLastWorked/:companyId/:adminId', adminController.showLastWorked
 // As an admin I should be able to view the investors' feedback so that I can make the right improvements to the service
 router.get('/getFeedback/:id', adminController.getFeedback)
 
-router.get('/showLastWorked/:companyId/:adminId', async (req, res) => {
-  try {
-    const adminId = req.params.adminId
-    const admin = await Admin.findById(adminId)
-    if (!admin) { // make sure that the one accessing the page is an admin
-      return res.status(400).json({
-        status: 'Error',
-        message: 'Access denied'
-      })
-    }
-    const companyId = req.params.companyId
-    const requestedCase = await Company.findById(companyId)
-    if (!requestedCase) { 
-      return res.status(400).json({
-        status: 'Error',
-        message: 'Case not found'
-      })
-    }
-    const result = []
-    if (requestedCase.form.acceptedByLawyer !== -1) {
-      const lawyer = await Lawyer.findById(requestedCase.form.lawyerID)
-      result.push('Lawyer: ' + lawyer.fullName)
-    }
-    if (requestedCase.form.acceptedByReviewer !== -1) {
-      const reviewer = await Reviewer.findById(requestedCase.form.reviewerID)
-      result.push('Reviewer: ' + reviewer.fullName)
-    }
-    return res.json({
-      status: 'Success',
-      message: `This case was last worked on by:`,
-      data: result
-    })
-  } catch (error) {
-    console.log(error)
-  }
-})
+router.get('/showLastWorked/:companyId/:adminId', adminController.showLastWorked)
 
 module.exports = router
