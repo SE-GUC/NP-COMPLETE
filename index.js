ba8499407959a64615189a95a777e91e2addea26
Lujine Elfeky <lujineelfeky@gmail.com>
2019-04-06 11:57:15 +0200

    Added error handlers and cors access

--- index.js
+++ index.js
@@ -50,10 +50,51 @@ app.use('/api/tasks', tasks)
 app.use('/api/companyTypes', companyTypes)
 app.use('/api/users', users)
 
-// Handling 404
-app.use((req, res) => {
-  res.status(404).send({ err: 'We can not find what you are looking for' })
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
 })
+
+// 500 internal server error handler
+app.use((err, _req, res, next) => {
+  if (err.statusCode === 400) {
+    return next(err)
+  }
+
+  return res.status(500).json({
+    data: null,
+    err: process.env === 'production'
+      ? null
+      : err,
+    msg: process.env === 'production'
+      ? 'Error!'
+      : '500 Internal Server Error'
+  })
+})
+
+// 400 error handler
+app.use((err, _req, res, next) => {
+  if (err.statusCode === 404) {
+    return next()
+  }
+
+  return res.status(400).json({
+    data: null,
+    err: process.env === 'production'
+      ? null
+      : err,
+    msg: '400 Bad Request'
+  })
+})
+
+// 404 error handler
+app.use((_req, res) => res.status(404)
+  .json({
+    data: null,
+    status: 'Error',
+    msg: 'Error 404: We can not find what you are looking for'
+  }))
 
 const port = process.env.PORT | 8000
 app.listen(port, () => { console.log(`Server is up and running on port ${port}`) })
