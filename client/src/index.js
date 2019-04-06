a3a08906287b9993cd4b6c479c9db0a59fbb533e
Shiko <ahmedelsherif98j@gmail.com>
2019-04-06 22:23:22 +0200

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import ViewCases from './ViewCases'
 import App from './App'
 
-if (window.location.pathname === '/companies' || window.location.pathname === '/companies/') {
-  ReactDOM.render(<ViewCases />, document.getElementById('root'))
-} else {
-  ReactDOM.render(<App />, document.getElementById('root'))
-}
+ReactDOM.render(<App />, document.getElementById('root'))
