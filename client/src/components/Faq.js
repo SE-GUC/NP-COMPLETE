842d180ddf3989fef7260e2695054a709d312ee6
MohamedTarawa <mohamed.tarawa@student.guc.edu.eg>
2019-04-06 19:39:33 +0200

    Create Faq.js

---
+++ client/src/components/Faq.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import Qa from './Qa'
+
+class Faq extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      faqs: [{
+        title: "Who is me ?",
+          body: "Mohamed Yasser"
+      },
+      {
+          title: "How is me ?",
+          body: "Better than you :)"
+      },
+      {
+          title: "How old is me ?",
+          body: "20 and turning 21 in 2 months .. Wohoo .. *Birthday celebrating emoji*"
+      },
+      {
+          title: "What the hell are these questions ?",
+          body: "Dummy questions till we find some interesting FAQs *wink*"
+      }]
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <section className="section">
+            <h1 className="title">FAQ</h1>
+
+            <div className="columns">
+              {this.state.faqs.map(faq => (
+                <div>
+                <Qa title={faq.title} body={faq.body} />
+                <br />
+              </div>
+              ))}
+            </div>
+          </section>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Faq;
