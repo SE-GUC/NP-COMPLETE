8b38442daf074041b63d4d98ef32a141739fa23b
Lujine Elfeky <lujineelfeky@gmail.com>
2019-04-07 18:25:14 +0200

    Displays comment

--- client/src/pages/investorPages/ViewRejectedForms.js
+++ client/src/pages/investorPages/ViewRejectedForms.js
@@ -23,31 +23,51 @@ class ViewForm extends Component {
           const formItems = []
           var i
           for (i = 0; i < resultArr.length; i++) {
-            formItems.push(resultArr[i].data)
+            formItems.push(
+              // resultArr[i].data
+              {
+                data: resultArr[i].data,
+                comment: resultArr[i].comment
+              }
+              
+              )
           }
           this.setState({ formItems: formItems, loading: false })
         }
       )
       .catch(error => {
         console.log(error['response'].data)
-        this.setState({ errorMessage: error['response'].data })
-        this.setState({ error: true })
+        this.setState({error: true, loading: false, errorMessage: error['response'].data })
       })
   }
   render () {
-    if (this.state.error) {
+    console.log(this.state)
+    console.log(this.props)
+    if(this.state.loading) {
+      return <h1> Loading </h1>
+    }
+    if (this.state.loading === false && this.state.error) {
       return (
         <div>
           <h1> Error </h1>
         </div>
       )
     }
+
+    if(this.state.loading === false && this.state.formItems.length === 0){
+      return (
+        <h1> No companies to display</h1>
+      )
+    }
     return (
-      <div>
-        <div className='Forms'>
-          <p> {this.state.formItems} </p>
-        </div>
-      </div>
+      this.state.formItems.map( x => (
+           <div>
+              <p> Form data: </p>
+              <p> {x.data} </p>
+              <p> Comment </p>
+             <p> {x.comment }</p>
+           </div>
+         ))
     )
   }
 }
