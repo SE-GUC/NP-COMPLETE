62aa82446e4fc525a8ece83b4bc3e60220fed068
M. Farid <mohamed.aiman@student.guc.edu.eg>
2019-04-12 00:16:34 +0200

    Fixed Index Deprecationwarning

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const db = require('./config/keys').mongoURI
 
 // Connect to mongo
 mongoose
-  .connect(db, { useNewUrlParser: true, useFindAndModify: false })
+  .connect(db, { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.log(err))
 
