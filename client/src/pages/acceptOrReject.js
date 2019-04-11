16e34af05a15ba9c410a23d8021bbd81251d27fe
Mohamed-Hosam <medo.hosam1@yahoo.com>
2019-04-11 17:37:35 +0200

    fixed user story 4.01 and 4.03

--- client/src/pages/acceptOrReject.js
+++ client/src/pages/acceptOrReject.js
@@ -7,31 +7,60 @@ import Axios from 'axios';
 class acceptOrReject extends Component {
 
   state = {
-    forms: [
-      {
-        id: 1,
-        title: 'balabizo1'
-      }
-    ]
+    loading: true,
+    forms: []
   }
-  accept = () =>{
+
+  componentDidMount() {
+    const { companyId } = this.props.match.params 
+    this._isMounted = true
+    // console.log("form from axios starts")
+    this.setState({loading: true})
+    Axios
+    
+    .get(`http://localhost:8000/api/companies/${companyId}`)
+    // .then(console.log("form from axios starts"))
+    // .then(res => this.setState({ forms: res.data.data }))
+    .then(res => this.setState({forms : 
+      (res.data.data.form.acceptedByReviewer !== -1)?
+      []
+      :
+    (res.data.data.form )
+    }))
+    .then(res => this.setState({loading: false}))
+    .then(res => console.log(this.state.forms ))
+
+    // .then(console.log("form from axios ends"))
+    .catch(err => {
+      console.log(err)
+    })
+    // console.log("form from axios ends")
+    console.log(this.state)
+}
+
+
+  accept = (e , root) =>{
+    e.preventDefault()
     const { reviewerId , companyId } = this.props.match.params
     Axios
     .put(`http://localhost:8000/api/reviewers/decideAnApplication/${reviewerId}/${companyId}`  , {decision: true})
     .then(res => {
       console.log(res.data.data)  
+      this.setState({ forms: [] })
     })
     .catch(err => {
       console.log(err)
     })
   }
-  reject = () =>{
+  reject = (e , root) =>{
+    e.preventDefault()
     const { reviewerId , companyId } = this.props.match.params
 
     Axios
     .put(`http://localhost:8000/api/reviewers/decideAnApplication/${reviewerId}/${companyId}`  , {decision: false})
     .then(res => {
       console.log(res.data.data)
+      this.setState({ forms: [] })
     })
     .catch(err => {
       console.log(err)
@@ -42,10 +71,12 @@ class acceptOrReject extends Component {
     return (
       <div className="App">
       <Header/>
-      <Forms forms = {this.state.forms}
+      {this.state.loading? <h1>loading please be patient</h1>: 
+      <Forms forms = {[this.state.forms]}
          accept = {this.accept}
          reject = {this.reject}
-      />      
+         root = {this}
+      />      }     
       </div>
     );
   }
