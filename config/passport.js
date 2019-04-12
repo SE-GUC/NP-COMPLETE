d16728bae636ac09f3b96ba04f3974cab61a0bca
Adeeel98 <mohamed.naguibahmed@student.guc.edu.eg>
2019-04-12 20:15:26 +0200

    adding passport

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,11 @@
 const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt
 const mongoose = require('mongoose')
-const User = mongoose.model('users')
+const Admin = mongoose.model('Admin')
+const Lawyer = mongoose.model('Lawyer')
+const Reviewer = mongoose.model('Reviewer')
+const Investor = mongoose.model('Investor')
+
 const tokenKey = require('./keys').secretOrKey
 
 let opts = {}
@@ -10,8 +14,20 @@ opts.secretOrKey = tokenKey
 
 module.exports = passport => {
   passport.use(new JwtStrategy(opts, async (jwtPayload, done) => {
-    const currentUser = await User.findById(jwtPayload.id)
-    if (currentUser) return done(null, currentUser)
-    return done(null, false)
+    const currentAdmin = await Admin.findById(jwtPayload.id)
+    const currentLawyer = await Lawyer.findById(jwtPayload.id)
+    const currentReviewer = await Reviewer.findById(jwtPayload.id)
+    const currentInvestor = await Investor.findById(jwtPayload.id)
+    if (currentAdmin) {
+      return done(null, currentAdmin)
+    } else if ((currentLawyer)) {
+      return done(null, currentLawyer)
+    } else if (currentReviewer) {
+      return done(null, currentReviewer)
+    } else if (currentInvestor) {
+      return done(null, currentInvestor)
+    } else {
+      return done(null, false)
+    }
   }))
 }
