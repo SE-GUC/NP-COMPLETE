e278dc91cfb14e828a36ced7033fabb526a781c5
Shiko <ahmedelsherif98j@gmail.com>
2019-04-12 03:44:40 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,26 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Header from './components/generic/Header'
-import ViewForm from './pages/investorPages/ViewRejectedForms'
-import Tracker from './pages/investorPages/Tracker'
-import ViewCases from './pages/ViewCases'
-import Faqs from './pages/investorPages/Faqs'
-import Ejournals from './pages/CompaniesEjournals'
+import './App.css'
+import Admin from './pages/Admin'
+import Lawyer from './pages/Lawyer'
 import Login from './pages/LoginPage'
+import Investor from './pages/Investor'
+import Reviewer from './pages/Reviewer'
+import React, { Component } from 'react'
+import ViewCases from './pages/ViewCases'
 import AddComment from './pages/AddComment'
-import Register from './pages/investorPages/Register'
+import Faqs from './pages/investorPages/Faqs'
+import Header from './components/generic/Header'
+import Ejournals from './pages/CompaniesEjournals'
+import Tracker from './pages/investorPages/Tracker'
 import acceptOrReject from './pages/acceptOrReject'
 import AdminViewCases from './pages/AdminViewCases'
 import LawyerViewCases from './pages/LawyerViewCases'
+import Register from './pages/investorPages/Register'
 import ReviewerViewCases from './pages/ReviewerViewCases'
 import AdminsViewMyTasks from './pages/AdminsViewMyTasks'
 import LawyersViewMyTasks from './pages/LawyersViewMyTasks'
+import ViewForm from './pages/investorPages/ViewRejectedForms'
 import ReviewersViewMyTasks from './pages/ReviewersViewMyTasks'
-
-import './App.css'
-import Admin from './pages/Admin'
-import Investor from './pages/Investor'
-import Lawyer from './pages/Lawyer'
-import Reviewer from './pages/Reviewer'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends Component {
   render () {
@@ -42,43 +41,43 @@ class App extends Component {
               </React.Fragment>
             )} />
 
-            <Route exact path='/investors/viewRejected/:id' component={ViewForm} />
+            <Route exact path='/LogIn' component={Login} />            
 
-            <Route exact path='/investors/tracker/:id' component={Tracker} />
+            <Route exact path='/investors/Faqs' component={Faqs} />            
 
-            <Route exact path='/admins/ViewCases' component={ViewCases} />
+            <Route exact path='/admins/deleteAdmin' component={Admin} />            
 
-            <Route exact path='/investors/Faqs' component={Faqs} />
+            <Route exact path='/admins/deleteLawyer' component={Lawyer} />            
 
-            <Route exact path='/companies/Ejournals' component={Ejournals} />
+            <Route exact path='/admins/ViewCases' component={ViewCases} />            
 
-            <Route exact path='/LogIn' component={Login} />
+            <Route exact path='/investors/Register' component={Register} />            
+
+            <Route exact path='/companies/Ejournals' component={Ejournals} />            
+
+            <Route exact path='/investors/tracker/:id' component={Tracker} />            
+
+            <Route exact path='/admins/deleteInvestor' component={Investor} />            
+
+            <Route exact path='/admins/deleteReviewer' component={Reviewer} />            
+
+            <Route exact path='/investors/viewRejected/:id' component={ViewForm} />            
+
+            <Route exact path='/admins/viewAllCases/:id' component={AdminViewCases} />            
+
+            <Route exact path='/admins/viewMyTasks/:id' component={AdminsViewMyTasks} />            
+
+            <Route exact path='/lawyers/viewAllCases/:id' component={LawyerViewCases} />            
+
+            <Route exact path='/lawyers/viewMyTasks/:id' component={LawyersViewMyTasks} />            
+
+            <Route exact path='/reviewers/viewAllCases/:id' component={ReviewerViewCases} />            
+
+            <Route exact path='/reviewers/viewMyTasks/:id' component={ReviewersViewMyTasks} />            
+
+            <Route exact path='/lawyers/addComment/:lawyerId/:companyId' component={AddComment} />            
 
             <Route exact path='/reviewers/acceptOrReject/:reviewerId/:companyId' component={acceptOrReject} />
-
-            <Route exact path='/lawyers/addComment/:lawyerId/:companyId' component={AddComment} />
-
-            <Route exact path='/investors/Register' component={Register} />
-
-            <Route exact path='/admins/deleteAdmin' component={Admin} />
-
-            <Route exact path='/admins/deleteLawyer' component={Lawyer} />
-
-            <Route exact path='/admins/deleteReviewer' component={Reviewer} />
-
-            <Route exact path='/admins/deleteInvestor' component={Investor} />
-
-            <Route exact path='/admins/viewAllCases/:id' component={AdminViewCases} />
-
-            <Route exact path='/admins/viewMyTasks/:id' component={AdminsViewMyTasks} />
-
-            <Route exact path='/lawyers/viewAllCases/:id' component={LawyerViewCases} />
-
-            <Route exact path='/lawyers/viewMyTasks/:id' component={LawyersViewMyTasks} />
-            
-            <Route exact path='/reviewers/viewAllCases/:id' component={ReviewerViewCases} />
-            
-            <Route exact path='/reviewers/viewMyTasks/:id' component={ReviewersViewMyTasks} />
             
           </div>
         </div>
