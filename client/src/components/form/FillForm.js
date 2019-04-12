4ca59e901e6b04010fd85a751409b97a1d9380f3
mostafa-elhaiany <mostafa_elhaiany@outlook.com>
2019-04-12 18:50:24 +0200

    formatted code

--- client/src/components/form/FillForm.js
+++ client/src/components/form/FillForm.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react'
 import Section from './Section'
-import {Form} from 'reactstrap'
+import { Form } from 'reactstrap'
 
 class FillForm extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            form: props.form
-        }
+  constructor (props) {
+    super(props)
+    this.state = {
+      form: props.form
     }
+  }
 
-    render() {
-        const renderSections = this.state.form.sections.map((section, i) => {
-            return <Section number={i} section={section} />
-        })
+  render () {
+    const renderSections = this.state.form.sections.map((section, i) => {
+      return <Section number={i} section={section} />
+    })
 
-        return (
-            <Form>
-                {renderSections}
-            </Form>
-        )
-    }
+    return (
+      <Form>
+        {renderSections}
+      </Form>
+    )
+  }
 }
 
 export default FillForm
--- client/src/components/form/TextField.js
+++ client/src/components/form/TextField.js
@@ -1,91 +1,62 @@
-import React, {Component} from 'react'
-import { Col, FormGroup, Label, Input, FormText, FormFeedback} from 'reactstrap';
+import React, { Component } from 'react'
+import { Col, FormGroup, Label, Input, FormText, FormFeedback } from 'reactstrap'
 
 class TextField extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            fieldName: props.fields.fieldName,
-            required: props.fields.required,
-            inputType: props.fields.inputType,       
-            validations: props.fields.validations,
-            validate: '',
-            errorMessage: ''
-        }
-        this.validate = this.validate.bind(this)
-        // this.handleChange = this.handleChange.bind(this)
+  constructor (props) {
+    super(props)
+    this.state = {
+      fieldName: props.fields.fieldName,
+      required: props.fields.required,
+      inputType: props.fields.inputType,
+      validations: props.fields.validations,
+      validate: '',
+      errorMessage: ''
     }
+    this.validate = this.validate.bind(this)
+  }
 
-    validate(e) {
-        if(!this.state.validations)
-            return
-        const min = this.state.validations[0].min
-        if(Number(e) < min){
-            this.setState.validate = 'danger'
-            this.setState.errorMessage = `Minimum amount is ${min}`
-        } else {
-            this.setState.validate = 'success'
-            this.setState.errorMessage = ''
-        }
-        console.log(`e: ${e}, type of e ${typeof e} min: ${this.state.validations[0].min}, state: ${this.setState.validate}, errorM: ${this.setState.errorMessage}`)
-        // validations.map((x) => {
-        //     switch(Object.keys(x)[0]) {
-        //         case 'min':
-        //             if(e < x.Object.keys(x)[0]){
-        //                 this.setState.validate = 'danger'; 
-        //                 this.setState.errorMessage = `Minimum amount is ${x.Object.keys(x)[0]}`;
-        //                 break;
-        //             }
-        //         case 'max': 
-        //             if(e < x.Object.keys(x)[0]){
-        //                 this.setState.validate = 'safe'; 
-        //                 this.setState.errorMessage = `Maximum amount is ${x.Object.keys(x)[0]}`; 
-        //                 break;
-        //             }
-        //     }
-
-        // })
+  validate (e) {
+    if (!this.state.validations) {
+      return
     }
-
-    // handleChange = async (event) => {
-    //     const { target } = event;
-    //     const value = target.type === 'checkbox' ? target.checked : target.value;
-    //     const { name } = target;
-    //     await this.setState({
-    //       [ name ]: value,
-    //     });
-    // }
-
-    render(){
-        return(
-            <FormGroup row> 
-                <Label for={this.state.fieldName} sm={2}> {this.state.fieldName} </Label>
-                <Col sm={10}>
-                    <Input type={this.state.inputType} 
-                        name={this.state.fieldName} 
-                        id={this.state.fieldName} 
-                        placeholder={this.state.fieldName} 
-                        valid={ this.state.validate === 'success' }
-                        invalid={ this.state.validate === 'danger' }
-                        onChange= {e => {
-                            const { target } = e;
-                            const value = target.type === 'checkbox' ? target.checked : target.value;
-                            this.validate(value)
-                            console.log(this.valid)
-                        }}
-                    />
-                    <FormFeedback valid>
+    const min = this.state.validations[0].min
+    if (Number(e) < min) {
+      this.setState({ validate: 'danger', errorMessage: `Minimum amount is ${min}` })
+    } else {
+      this.setState({ validate: 'safe', errorMessage: '' })
+    }
+    console.log(`e: ${e}, type of e ${typeof e} min: ${this.state.validations[0].min}, state: ${this.setState.validate}, errorM: ${this.setState.errorMessage}`)
+  }
+  render () {
+    return (
+      <FormGroup row>
+        <Label for={this.state.fieldName} sm={2}> {this.state.fieldName} </Label>
+        <Col sm={10}>
+          <Input type={this.state.inputType}
+            name={this.state.fieldName}
+            id={this.state.fieldName}
+            placeholder={this.state.fieldName}
+            valid={this.state.validate === 'safe'}
+            invalid={this.state.validate === 'danger'}
+            onChange={e => {
+              const { target } = e
+              const value = target.type === 'checkbox' ? target.checked : target.value
+              this.validate(value)
+              console.log(this.valid)
+            }}
+          />
+          <FormFeedback valid>
                         All good!
-                    </FormFeedback>
-                    <FormFeedback>
-                        {`Uh Oh! ${this.state.errorMessage}`}
-                    </FormFeedback>
-                    <FormText>This is most likely your {this.state.fieldName}.</FormText>
-                </Col>
-          </FormGroup>
-            
-        )
-    }
+          </FormFeedback>
+          <FormFeedback>
+            {`Uh Oh! ${this.state.errorMessage}`}
+          </FormFeedback>
+          <FormText>This is most likely your {this.state.fieldName}.</FormText>
+        </Col>
+      </FormGroup>
+
+    )
+  }
 }
 
 export default TextField
