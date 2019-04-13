e4ade864128e3ed02b030823e37c99c982a7204a
Lujine Elfeky <lujineelfeky@gmail.com>
2019-04-13 22:50:47 +0200

    Commented log in functionality until fixes

--- client/src/pages/investorPages/InvestorLogin.js
+++ client/src/pages/investorPages/InvestorLogin.js
@@ -17,20 +17,20 @@ class InvestorLogin extends Component {
      // These are the steps to store the user token at the frontend
   // however it involves alot of modifications in the backend
 
-  loginInvestor = (email, password) => {
-//     console.log(email)
-//     console.log(password)
-	axios.post('http://localhost:8000/api/investors/login', {email: email, password: password})
-	.then( res => {
-		const { token } = res.data
-		localStorage.setItem('jwtToken', token)
-		 if(token) 
-        axios.defaults.headers.common['Authorization'] = token
-    else 
-     delete axios.defaults.headers.common['Authorization'] 
-	})
-	.catch(err => console.log('error'))	
-	}
+//   loginInvestor = (email, password) => {
+// //     console.log(email)
+// //     console.log(password)
+// 	axios.post('http://localhost:8000/api/investors/login', {email: email, password: password})
+// 	.then( res => {
+// 		const { token } = res.data
+// 		localStorage.setItem('jwtToken', token)
+// 		 if(token) 
+//         axios.defaults.headers.common['Authorization'] = token
+//     else 
+//      delete axios.defaults.headers.common['Authorization'] 
+// 	})
+// 	.catch(err => console.log('error'))	
+// 	}
 
   render () {
     return (
