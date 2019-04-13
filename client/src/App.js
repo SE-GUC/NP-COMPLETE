8ea59c9e6720c343326a5084350b039ff3b99b9d
M. Farid <mohamed.aiman@student.guc.edu.eg>
2019-04-13 23:45:52 +0200

    Added investor landing page - missing functionality

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,7 @@ import ViewForm from './pages/investorPages/ViewRejectedForms'
 import ReviewersViewMyTasks from './pages/ReviewersViewMyTasks'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import AdminLandingPage from './pages/adminPages/AdminLandingPage'
+import InvestorLandingPage from './pages/investorPages/InvestorLandingPage'
 
 
 class App extends Component {
@@ -46,7 +47,9 @@ class App extends Component {
 
             <Route exact path='/investors/Faqs' component={Faqs} />            
 
-            <Route exact path='/admin' component={AdminLandingPage} />            
+            <Route exact path='/admin' component={AdminLandingPage} />
+
+            <Route exact path='/investor' component={InvestorLandingPage} />            
 
             <Route exact path='/admins/deleteAdmin' component={Admin} />            
 
--- client/src/pages/adminPages/AdminLandingPage.js
+++ client/src/pages/adminPages/AdminLandingPage.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
-
 export class AdminLandingPage extends Component {
 
   render() {
@@ -14,7 +13,7 @@ export class AdminLandingPage extends Component {
               <Card.Text>
                 View all cases in the system
               </Card.Text>
-             <Button variant='primary' href='http://localhost:3000/admins/viewAllCases/5cafc730199ec12808cac488'>Click me</Button>
+             <Button variant='primary' href='/admins/viewAllCases/5cafc730199ec12808cac488'>Click me</Button>
             </Card.Body>
           </Card>
           <Card>
@@ -23,23 +22,23 @@ export class AdminLandingPage extends Component {
               <Card.Text>
                 Publish a company
                 <br/>
-                lsa lujine bt3mlha
+                lsa yasser by3melha
               </Card.Text>
-             <Button variant='primary' href='http://localhost:3000/admins/viewAllCases/5cafc730199ec12808cac488'>Click me</Button>
+             <Button variant='primary' href='/admins/viewAllCases/5cafc730199ec12808cac488'>Click me</Button>
             </Card.Body>
           </Card>
           <Card>
             <Card.Body>
-              <Card.Title><h4>Account Page</h4></Card.Title>
+              <Card.Title><h4>Accounts Page</h4></Card.Title>
               <Card.Text>
                 View my account information
                 <br/>
                 do we have one aslan ??????????????????!!!!!!!!!!!!!!!!!!!!!!
               </Card.Text>
-             <Button variant='primary' href='http://localhost:3000/admins/deleteAdmin'>Admins</Button>
-             <Button variant='primary' href='http://localhost:3000/admins/deleteLawyer'>Lawyers</Button>
-             <Button variant='primary' href='http://localhost:3000/admins/deleteReviewer'>Reviewers</Button>
-             <Button variant='primary' href='http://localhost:3000/admins/deleteInvestor'>Investors</Button>
+             <Button variant='primary' href='/admins/deleteAdmin'>Admins</Button>
+             <Button variant='primary' href='/admins/deleteLawyer'>Lawyers</Button>
+             <Button variant='primary' href='/admins/deleteReviewer'>Reviewers</Button>
+             <Button variant='primary' href='/admins/deleteInvestor'>Investors</Button>
             </Card.Body>
           </Card>
       </div>
