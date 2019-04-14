ff36d62d48a4c960fee85f10398b752e8073ae70
MohamedTarawa <mohamed.tarawa@student.guc.edu.eg>
2019-04-14 14:52:38 +0200

    Update Company.js

--- client/src/components/company/Company.js
+++ client/src/components/company/Company.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react'
-import CItem from './CItem'
+import React, { Component } from "react";
+import CItem from "./CItem";
 
 export class Company extends Component {
-  render () {
-    return this.props.forms.map((form) => (
+  render() {
+    return this.props.forms.map(form => (
       <CItem key={form.id} form={form} publish={this.props.publish} />
-    ))
+    ));
   }
 }
 
-export default Company
+export default Company;
