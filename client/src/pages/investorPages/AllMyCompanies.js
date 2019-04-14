cbb5c1f2aaeb353c2fc7e3478dc74f1cf3077b0d
Lujine Elfeky <lujineelfeky@gmail.com>
2019-04-14 17:59:42 +0200

    Modified My companies page to use local storage

--- client/src/pages/investorPages/AllMyCompanies.js
+++ client/src/pages/investorPages/AllMyCompanies.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import Mycompanies from '../../components/MyCompanies'
 export class AllMyCompanies extends Component {
-
-  render() {
+  render () {
     return (
       <div>
         <div className='AllMyCompanies'>
-        <Mycompanies keys id={this.props.match.params.id} />
-      </div>
+          <Mycompanies keys id={localStorage.getItem('id')} />
+          {/* {this.props.match.params.id} */}
+        </div>
       </div>
     )
   }
