6b5c4c7c63aef40ab0b42716de3aa2cbb6df2e69
MohamedTarawa <mohamed.tarawa@student.guc.edu.eg>
2019-04-14 14:52:34 +0200

    Update CItem.js

--- client/src/components/company/CItem.js
+++ client/src/components/company/CItem.js
@@ -1,38 +1,39 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
 
 export class CItem extends Component {
-
-getStyle = () =>{
+  getStyle = () => {
     return {
-        background: 'f4f4f4',
-        padding: '10px',
-        borderBottom: '1px #ccc dotted'
-    }
-
-}
+      background: "f4f4f4",
+      padding: "10px",
+      borderBottom: "1px #ccc dotted"
+    };
+  };
 
   render() {
     return (
-      <div style ={this.getStyle()}>
-      <p>
-      {this.props.form.name}
-      <button onClick= {this.props.publish.bind(this,this.props.form._id)} style= {btnStyle} >publish </button>
-    </p>
-    </div>
-      
-    )
+      <div style={this.getStyle()}>
+        <p>
+          {this.props.form.name}
+          <button
+            onClick={this.props.publish.bind(this, this.props.form._id)}
+            style={btnStyle}
+          >
+            publish
+          </button>
+        </p>
+      </div>
+    );
   }
 }
 const btnStyle = {
-  background: '#ff0000',
-  color: 'fff',
-  border: 'none',
-  padding: '5px 8px',
-  cursor: 'pointer',
-  float: 'right',
+  background: "#ff0000",
+  color: "fff",
+  border: "none",
+  padding: "5px 8px",
+  cursor: "pointer",
+  float: "right",
   fontSize: 20,
-  borderRadius: '50%'
-}
+  borderRadius: "50%"
+};
 
-export default CItem
+export default CItem;
