fb83d946bb853d9b81f6c3c784b6eb0c222d3883
Lujine Elfeky <lujineelfeky@gmail.com>
2019-04-18 08:27:23 +0200

    Initial guard router code

---
+++ client/src/utilities/InvestorPrivateRoute.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { logout } from '../actions/authActions'
+import store from '../store'
+
+const InvestorPrivateRoute = ({ component: Component, ...rest }) => (
+  
+  <Route {...rest} render={(props) => (
+    // console.log(store.getState().auth.isLoggedIn)
+    store.getState().auth.isLoggedIn === true
+      ? (<Component {...props} />)
+      : props.history.pushState('/investors/login')
+      // <Redirect to={{
+      //   pathname: '/investors/login',
+      //   state: { from: props.location }
+      // }} />
+  )}
+  />
+
+)
+InvestorPrivateRoute.propTypes = {
+  logout: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+  isLoggedIn: state.auth.isLoggedIn,
+  loggedUser: state.auth.loggedUser
+})
+
+export default connect(mapStateToProps, { logout })(InvestorPrivateRoute)
