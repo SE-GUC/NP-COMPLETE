596a6d61fc9ec0a70ca2a485d44e08ad66016888
Mohamed-Hosam <medo.hosam1@yahoo.com>
2019-04-21 18:09:24 +0200

    moved give feedback to mycoompanies page

--- client/src/App.js
+++ client/src/App.js
@@ -41,7 +41,6 @@ import ReviewForm from './pages/ReviewForm'
 import { Container } from 'react-bootstrap'
 import NavBar from './components/generic/NavbarGafi'
 import getFeedback from './pages/getFeedback'
-import giveFeedback from './pages/giveFeedback'
 
 if (localStorage.jwtToken) {
   setAuthToken(localStorage.jwtToken)
@@ -136,9 +135,6 @@ class App extends Component {
             <Route exact path='users/reviewForm' component={ReviewForm} />
 
             <Route exact path='/admins/getFeedback' component={getFeedback} />
-
-            <Route exact path='/investors/reviewOnlineService/:companyId/:investorId' component={giveFeedback} />
-
 
           </Container>
         </Router>
--- client/src/components/MyCompanyCard.js
+++ client/src/components/MyCompanyCard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Card from 'react-bootstrap/Card'
 import CardDeck from 'react-bootstrap/CardDeck'
+import Axios from 'axios';
 import {
   Collapse,
   Navbar,
@@ -18,7 +19,11 @@ import { Button } from 'react-bootstrap'
 export class MyCompaniesCard extends Component {
   constructor (props) {
     super(props)
+    this.toggle = this.toggle.bind(this)
+
     this.state = {
+      collapse: false,
+      feedback: ''
     }
   }
   componentDidMount () {
@@ -26,6 +31,25 @@ export class MyCompaniesCard extends Component {
       this.setState({ [key]: this.props.data[key] })
     })
   }
+  toggle () {
+    this.setState(state => ({ collapse: !state.collapse }))
+  }
+  submit = () => {
+    if(this.submit.length !== 0){
+    this.setState({ feedback: this.submit })
+    const investorId = "5cba2b7864df6c23283bea53"  //need to change this to redux
+    const companyId = this.props.data._id
+    console.log(investorId)
+    console.log(companyId)
+    Axios
+    .put(`/api/investors/reviewOnlineService/${companyId}/${investorId}` , {feedback: this.submit})
+    .then(res => this.setState({loading: false , done: true}))
+    .catch(err => {
+        console.log(err)
+      })
+      this.toggle()
+    }
+}
   render () {
     return (
       <CardDeck>
@@ -43,7 +67,14 @@ export class MyCompaniesCard extends Component {
             </Card.Text>
             <Nav className='ml-auto' navbar>
               <NavItem>
-                <NavLink href = {`http://localhost:3000/investors/reviewOnlineService/` + this.state._id + `/:investorId`}>Give</NavLink>
+                <Button color='primary' onClick={this.toggle} style={{ marginBottom: '1rem' }}>Give Feedback</Button>
+                <Collapse isOpen={this.state.collapse}>
+                  <div class='App'>
+                    <label for='comment'>Comment:</label>
+                    <textarea class='form-control' rows='5' id='comment' onChange={(event) => { this.submit = event.target.value }} />
+                    <Button color='primary' onClick = { this.submit  } style={{ marginBottom: '1rem' }} > Submit Feedback </Button> 
+                  </div>
+                </Collapse>
               </NavItem>
             </Nav>
           </Card.Body>
