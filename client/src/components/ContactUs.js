ea0a1aba542eb269b1ba96ccc8ccdf88077680da
MohamedTarawa <mohamed.tarawa@student.guc.edu.eg>
2019-04-22 20:07:03 +0200

    Update ContactUs.js

--- client/src/components/ContactUs.js
+++ client/src/components/ContactUs.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 
 export class ContactUs extends Component {
+
+
   render() {
+    if(localStorage.getItem('language') === 'English'){
     return (
       <div>
         <h2>GOT QUESTIONS?</h2>
         <p>The easiest thing to do is to contact us on the following email<strong>gafiweb2019@gmail.com</strong></p>
       </div>
     )
+    }else{
+        return (
+            <div>
+              <h2>لديك أسالة ؟</h2>
+              <p><strong>gafiweb2019@gmail.com</strong> يمكنك التواصل معنا عن طريق البريد الالكتروني</p>
+            </div>
+          )
+    }
   }
 }
 
