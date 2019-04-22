f8c942ab88cddea68b40f3e056573c22a41f53d0
MohamedTarawa <mohamed.tarawa@student.guc.edu.eg>
2019-04-22 20:02:19 +0200

    Create ContactUs.js

---
+++ client/src/components/ContactUs.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class ContactUs extends Component {
+  render() {
+    return (
+      <div>
+        <h2>GOT QUESTIONS?</h2>
+        <p>The easiest thing to do is to contact us on the following email<strong>gafiweb2019@gmail.com</strong></p>
+      </div>
+    )
+  }
+}
+
+export default ContactUs
