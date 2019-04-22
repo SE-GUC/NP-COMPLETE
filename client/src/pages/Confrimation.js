cd014430e5ff138a3d05c795cbbc110adda8aa06
Mark-Micheal <markmichealhp@outlook.com>
2019-04-23 01:30:36 +0200

    Non-redirect test

--- client/src/pages/Confrimation.js
+++ client/src/pages/Confrimation.js
@@ -9,7 +9,8 @@ export class Confrimation extends Component {
       .get(`/api/${model}/confirmation/${emailToken}`)
       .then(res => alert(res.data.message))
       .catch(error => alert(error.response.data.message))
-    return <Redirect to='/login' />
+    // return <Redirect to='/login' />
+    return <div>res.data.message</div>
   }
 }
 
