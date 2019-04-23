201beeb7a27394e5b39b5f1235fff2dd34f2e9cd
Mark-Micheal <markmichealhp@outlook.com>
2019-04-23 04:46:59 +0200

    Trying again

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use((req, res, next) => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
 
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
